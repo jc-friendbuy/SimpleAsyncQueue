94c29a73c447a3f1858184de091858320d08c447
Juan Carlos Coto <jccotou@gmail.com>
2014-08-06 12:55:39 -0600

    First version

--- src/event_loop/EventLoop.py
+++ src/event_loop/EventLoop.py
@@ -1,24 +1,32 @@
-from threading import Lock
+from threading import Lock, Thread, Condition
 import time
 
 
 class EventLoop(object):
 
     def __init__(self):
-        self._lock = Lock()
         self._events = list()
+        self._thread = None
+        self._lock = Lock()
+        self._event_ready_condition = Condition(lock=self._lock)
+
+
 
     def publish(self, event):
         with self._lock:
             self._events.append(event)
 
     def start(self):
-        while True:
-            with self._lock:
-                try:
-                    self._process_event(self._events.pop())
-                except IndexError:
-                    time.sleep(0,1)
+        self._thread = Thread(target=self._process_events)
+
+    def _process_events(self):
+        do_process = True
+        while do_process:
+            with self._event_ready_condition as event_ready:
+                if event_ready.wait_for(len(self._events) > 0, timeout=0.01):
+
+
+
 
 
 
